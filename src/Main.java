62cf3a0a1e17eb574300c9940277bdd6be1f1021
Shaheer Ahmed Farooqui <shaheerahmedfarooqui758@gmail.com>
2024-02-05 01:58:32 +0500

    name change

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Azmat Fareed! 123");
+        System.out.println("Hello Azmat Fareed ");
 
 
     }
